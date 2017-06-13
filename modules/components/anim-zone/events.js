4bc5d3e987b7d75bcdb22cc66819f8d925246698
groot007 <kola.stani@gmail.com>
2017-06-13 15:55:03 +0300

    dropbox

--- modules/components/anim-zone/events.js
+++ modules/components/anim-zone/events.js
@@ -1,5 +1,19 @@
 function pluginEvents (root, $, name) {
-
+	$("#anim-element").draggable({ drag:function(event, ui){
+	  var left = $(".keyframe-tab.active input[name='left']");
+	  var top = $(".keyframe-tab.active input[name='top']");
+		if (!left.length){
+			$("<div class='group'> <label for=''>left</label><input value='0px' name='left'></input></div>")
+			.appendTo("#css-props .keyframe-tab.active");
+		}
+		if (!top.length){
+			$("<div class='group'> <label for=''>top</label><input value='0px' name='top'></input></div>")
+			.appendTo("#css-props .keyframe-tab.active");
+		}
+		left.val(ui.position.left + "px");
+		top.val(ui.position.top + "px");
+		left.trigger("input");
+	}});
 }
 
 
